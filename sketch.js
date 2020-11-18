5d23bb35c8894731613be8f552768829f148e6e9
Ace369 <aditi.chamarthy@gmail.com>
2020-11-18 15:39:33 -0800

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,59 @@
+var dog, happyDog, database, foodS, foodStock;
+
+function preload()
+{
+  happyDog = loadImage("images/dogImg1.png");
+  dogImg = loadImage("images/dogImg.png");
+}
+
+function setup() {
+  database = firebase.database();
+  createCanvas(500, 500);
+  
+  dog = createSprite(200, 200, 20, 20);
+  dog.addImage(dogImg);
+  dog.scale = 0.15;
+
+  foodStock = database.ref('Food');
+  foodStock.on("value", readStock);
+  
+}
+
+
+function draw() {  
+  background(46, 139, 87);
+
+  if(keyWentDown(UP_ARROW)) {
+
+    writeStock(foodS);
+    dog.addImage(happyDog);
+  }
+
+  drawSprites();
+  //add styles here
+
+  textSize(16);
+  fill("blue");
+  stroke("black");
+  text("Note: Press UP_ARROW Key To Feed Drago Milk", 100, 200);
+
+
+}
+
+function readStock(data){
+  foodS = data.val();
+}
+
+function writeStock(x){
+
+  if(x<=0){
+    x = 0;
+  }else{
+    x = x-1;
+  }
+
+  database.ref('/').update({
+    Food:x
+  })
+}
+
